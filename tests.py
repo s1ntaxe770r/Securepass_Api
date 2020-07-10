530ca5382a8922c13044eea4702b4d3dc50b5fc3
s1ntaxe770r <cyberdev@protonmail.com>
2020-07-10 02:27:58 +0100

    build and test fixes

---
+++ tests.py
@@ -0,0 +1,21 @@
+import unittest
+
+from app import app
+ 
+
+class BasicTestCase(unittest.TestCase):
+    def test_home(self):
+	    tester = app.test_client(self)
+	    response = tester.get('/generate', content_type='html/text')
+	    self.assertEqual(response.status_code, 200)
+
+class TestCase(unittest.TestCase):
+	def test_genid(self):
+		tester = app.test_client(self)
+		response = tester.get('/generate/12', content_type='html/text')
+		self.assertEqual(response.status_code,200)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
